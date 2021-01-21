6477cc525eea05362e0e34c566d38137c525edb8
DrVasile <drumeavasilexd@gmail.com>
2021-01-22 00:13:48 +0200

    Plot the trajectory on GMaps

--- geo_location_api/services.py
+++ geo_location_api/services.py
@@ -2,53 +2,52 @@ import os
 import gmplot
 import requests
 from requests import RequestException
+from numpy import random
 
 
 class CoordinatesPlotter:
     @staticmethod
     def plot_coordinates_on_map():
 
-        # apikey = ''
-        # gmap = gmplot.GoogleMapPlotter(37.766956, -122.448481, 14, apikey=apikey)
-        # gmap.marker(37.770776, -122.461689, color='cornflowerblue')
-        #
-        # attractions_lats, attractions_lngs = zip(*[
-        #     (37.769901, -122.498331),
-        #     (37.768645, -122.475328),
-        #     (37.771478, -122.468677),
-        #     (37.769867, -122.466102),
-        #     (37.767187, -122.467496),
-        #     (37.770104, -122.470436)
-        # ])
-        #
-        # gmap.scatter(attractions_lats, attractions_lngs, color='#3B0B39', size=40, marker=False)
-        #
-        # golden_gate_park = zip(*[
-        #     (37.771269, -122.511015),
-        #     (37.773495, -122.464830),
-        #     (37.774797, -122.454538),
-        #     (37.771988, -122.454018),
-        #     (37.773646, -122.440979),
-        #     (37.772742, -122.440797),
-        #     (37.771096, -122.453889),
-        #     (37.768669, -122.453518),
-        #     (37.766227, -122.460213),
-        #     (37.764028, -122.510347)
-        # ])
-        #
-        # gmap.polygon(*golden_gate_park, color='cornflowerblue', edge_width=10)
-        #
-        # gmap.draw('map.html')
-        # os.system('map.html')
+        apikey = ''
 
         try:
-            response = requests.get("https://api.thingspeak.com/channels/1273540/fields/1.json?api_key"
-                                    "=6VD0YT9MCMAD5ER3&results=2")
+            response = requests.get("")
             response.raise_for_status()
             print(response)
 
             response_json = response.json()
-            print('Latitude: ' + response_json["channel"]["latitude"])
-            print('Longitude: ' + response_json["channel"]["longitude"])
+            feeds = response_json['feeds']
+            lat_list = []
+            lon_list = []
+
+            for feed in feeds:
+                lat = float(feed['field2'])
+                lon = float(feed['field1'])
+                lat_list.append(lat)
+                lon_list.append(lon)
+
+            curr_lat = lat_list[-1]
+            curr_lon = lon_list[-1]
+
+            origin_lat = lat_list[0]
+            origin_lon = lon_list[0]
+            zoom_lvl = 16
+
+            gmap = gmplot.GoogleMapPlotter(origin_lat, origin_lon, zoom_lvl, apikey=apikey)
+
+            for i in range(100):
+                curr_lat += (random.rand() - 0.5) / 10000.0
+                lat_list.append(curr_lat)
+                curr_lon += (random.rand() - 0.5) / 10000.0
+                lon_list.append(curr_lon)
+
+            gmap.plot(lat_list, lon_list, edge_width=7, color='blue')
+            print(lat_list[0:5])
+            print(lon_list[0:5])
+
+            gmap.draw('map.html')
+            os.system('map.html')
+
         except RequestException:
             print('Request not satisfied!')
--- geo_location_api/settings.py
+++ geo_location_api/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$e@d_p_8dl^nksq&ra-=uqgsqi@9nk)3g9rf8iu0j8rq8w5gb1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
